5e3954e3c407b0c916b255738a7d8b4f36807401
rhauser14 <rhauser14@gmail.com>
2020-10-25 17:24:40 -0600

    Update Twilio.py

--- Twilio.py
+++ Twilio.py
@@ -28,7 +28,7 @@ st.write('Current Geolocation of Base stations and satellites')
 
 #Plotting map data
 map_data = pd.DataFrame(
-    np.random.randn(10000, 2) / [50, 50] + [37.76, -122.4],
+    np.random.randn(1000, 2) / [50, 50] + [1000, 1000]
     columns=['lat', 'lon'])
 
 st.map(map_data)
