3d58c624b973f164e5e02ea66f57848459b9ed84
rhauser14 <rhauser14@gmail.com>
2020-10-25 20:16:33 -0600

    Update vardata.py

--- vardata.py
+++ vardata.py
@@ -1,3 +1,9 @@
+import streamlit as st
+# To make things easier later, we're also importing numpy and pandas for
+# working with sample data.
+import numpy as np
+import pandas as pd
+
 #Plotting map data
 map_data = pd.DataFrame(
     np.random.randn(516, 2) * [20,45] + [0, 0],
